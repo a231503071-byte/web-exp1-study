a28802a75c8b0ac7b6dfe120121d0d122c734785
a231503071-byte <a231503071@gmail.com>
2026-04-19 18:41:50 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+
+window.EXP_CONFIG = {
+  experimentName: "实验1在线版：情绪诱发与词语记忆",
+  nWordsToUse: 20,
+  fixationMs: 500,
+  blankMs: 300,
+  wordMs: 2000,
+  interferenceSec: 45,
+  recallSec: 180,
+  imageSec: 3500,
+  imageISI: 300,
+  useSuggestedOrder: false,
+  randomizeWordSubset: true,
+  randomizePresentationOrder: true,
+  allowManualGroupChoice: false,
+  defaultGroupMode: "random",
+  requireFullscreen: true,
+  webhookUrl: "https://你的域名/save_exp1.php",
+  codeServiceUrl: "https://你的域名/assign_code.php",
+  webhookTimeoutMs: 12000
+};
